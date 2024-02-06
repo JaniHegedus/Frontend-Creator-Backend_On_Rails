b7854fca0e263f79b0326861fd64243ee547979c
Hegedüs János <hegedusjanos2002@gmail.com>
2024-02-06 14:07:36 +0100

    hotfix

--- app/controllers/ai_controller.rb
+++ app/controllers/ai_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require_relative '../models/Ai/reviewer'
+require_relative '../models/ai_reviewer'
 require_relative '../Components/config'
 
 class AiController < ApplicationController
