6c6a289531fb36844e3c6dd411d4060445c87da3
Hegedüs János <hegedusjanos2002@gmail.com>
2024-02-06 13:50:11 +0100

    hotfix

--- app/Components/File/file_reader.rb
+++ app/Components/File/file_reader.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 require_relative 'file_writer'
+
 class FileReader
   def initialize(file_path)
     @file_path = file_path
   end
+
   def read_data
     if File.exist?(@file_path)
       puts "The file exists."
       File.read(@file_path)
     else
-      FileWriter.new(@file_path, "", "FileReader",type: "n").write_data_append
-      puts "The file is now Created at:"+ @file_path
+      FileWriter.new(@file_path, "", "FileReader", type: "n").write_data_append
+      puts "The file is now created at: #{@file_path}"
+      # Consider what to return here, e.g., empty string or nil
     end
+  rescue StandardError => e
+    # Handle other file reading errors, log them or re-raise
+    puts "An error occurred: #{e.message}"
+    # Depending on your use case, you might want to return a default value here
   end
 end
